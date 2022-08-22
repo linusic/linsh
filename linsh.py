61a150e84cdfbe470d387a6e5960f666234d8c75
lin <Cython_lin@cklin.top>
2022-08-23 01:36:11 +0800

    add std stream
    add stdout 1,  add stderr 2

--- linsh.py
+++ linsh.py
@@ -15,36 +15,18 @@ class LinSH(str):
         sp.run(self.init, shell=True)
 
     def __gt__(self, other):
+        sub_obj = sp.run(self.init, shell=True, encoding="utf-8", stderr=sp.PIPE, stdout=sp.PIPE)
+
         """ > """
         # return None, but directly print stdout and stderr
         if other is None:
             sp.run(self.init, shell=True, encoding="utf-8")
         # return (stderr + stdout), but not print
         elif other is Ellipsis:
-            return sp.run(self.init, shell=True, encoding="utf-8", stderr=sp.PIPE, stdout=sp.PIPE)
-        # # return stdout
-        # elif other == 1:
-        #     sp.run(self.init, shell=True, encoding="utf-8", stdout=sp.PIPE)
-        # # return stderr
-        # elif other == 2:
-        #     sp.run(self.init, shell=True, encoding="utf-8", stderr=sp.PIPE, )
-
-def main():
-    cmd = "ls -al"
-    c = LinSH(cmd)
-    # -c
-
-    cmd1 = "grep py"
-    cmd2 = "grep 123"
-
-    c | cmd1 | cmd2 > None          # return None, but directly print stdout and stderr
-
-    result = c | cmd1 | cmd2 > ...  # return <subprocess obj> but not print
-    print(result.stdout, result.stderr)
-
-    # in the future ...
-    # c | cmd1 | cmd2 > 1   # stdout
-    # c | cmd1 | cmd2 > 2   # stderr
-
-if __name__ == '__main__':
-    main()
+            return sub_obj
+        # # only return stdout, not print
+        elif other == 1:
+            return sub_obj.stdout
+        # # only return stderr, not print
+        elif other == 2:
+            return sub_obj.stderr
