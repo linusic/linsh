08b72d743565f2e57ca337fff8ee808b9b4e8e19
lin <Cython_lin@cklin.top>
2022-09-20 15:41:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='linsh',
-    version="0.0.2",
+    version="0.0.4",
     author="linusic",
     author_email='cython_lin@cklin.top',
     description="magic operator for sh",
