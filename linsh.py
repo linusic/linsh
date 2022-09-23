4e9be23035935eb722ec0f64316536cd5e3b55ba
lin <Cython_lin@cklin.top>
2022-09-23 23:02:01 +0800

    Update linsh.py

--- linsh.py
+++ linsh.py
@@ -1,5 +1,5 @@
-import argparse
-import subprocess as sp
+import subprocess
+
 
 class LinSH(str):
     def __init__(self, init: str):
@@ -10,17 +10,13 @@ class LinSH(str):
         self.init = f"{self.init} | {command}"
         return self
 
-    def __neg__(self):
-        """ - """
-        sp.run(self.init, shell=True)
-
     def __gt__(self, other):
-        sub_obj = sp.run(self.init, shell=True, encoding="utf-8", stderr=sp.PIPE, stdout=sp.PIPE)
-
         """ > """
+        sub_obj = subprocess.run(self.init, shell=True, universal_newlines=True, stderr=subprocess.PIPE,
+                                 stdout=subprocess.PIPE)
         # return None, but directly print stdout and stderr
         if other is None:
-            sp.run(self.init, shell=True, encoding="utf-8")
+            subprocess.run(self.init, shell=True, universal_newlines=True)
         # return (stderr + stdout), but not print
         elif other is Ellipsis:
             return sub_obj
@@ -31,22 +27,21 @@ class LinSH(str):
         elif other == 2:
             return sub_obj.stderr
 
-parser = argparse.ArgumentParser(description='', usage='python ___.py _ _ _ ...')
-parser.add_argument('_', type=str, nargs='+')
-args = parser.parse_args()
+    def __and__(self, other):
+        """ & """
+        sub_obj = subprocess.Popen(self.init, shell=True, universal_newlines=True, stderr=subprocess.PIPE,
+                                   stdout=subprocess.PIPE)
 
-___ = []
+        # return None, but directly print stdout and stderr
+        if other is None:
+            subprocess.Popen(self.init, shell=True, universal_newlines=True)
 
-for index, value in  enumerate(args._, start=1):
-    globals()[f"_{index}"] = value
-    ___.append(f"_{index}")
-
-for index, value in  enumerate(args._, start=-len(args._)):
-    globals()[f"__{-index}"] = value
-    ___.append(f"__{-index}")
-
-globals()["__"] = args._
-
-
-# __all__ = list(globals())
-__all__ = ___ + ["__"] + ["___"] + ["LinSH"]
+        # return (stderr + stdout), but not print
+        elif other is Ellipsis:
+            return sub_obj
+        # only return stdout, not print
+        elif other == 1:
+            return sub_obj.stdout
+        # only return stderr, not print
+        elif other == 2:
+            return sub_obj.stderr
